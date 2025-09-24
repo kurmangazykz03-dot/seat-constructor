0b8aea71bbc7d986609845fe4a2bd74f7927724e
Kurman <kurmangazy.kz03@gmail.com>
2025-09-24 23:51:45 +0500

    add HomePage.txs

---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <div className="bg-[#FAFAFA] w-full min-h-screen flex justify-center items-center">
+      <div className="bg-white w-[448px] h-[500px] rounded-[16px] drop-shadow-[0_0_15px_rgba(0,0,0,0.10)] flex items-center flex-col pt-[43px] pb-[107px] px-[48px]">
+        <div className="bg-[#E5E5E5] w-[64px] h-[64px] rounded-[16px] mb-[72px]" />
+        <span className="text-[#171717] text-[24px] mb-[56px]">Конструктор схемы</span>
+
+        <Link
+          to="/editor"
+          className="w-[352px] h-[56px] flex items-center justify-center bg-[#525252] text-white rounded mb-[16px] hover:bg-blue-500"
+        >
+          Редактировать
+        </Link>
+
+        <Link
+          to="/viewer"
+          className="w-[352px] h-[56px] flex items-center justify-center bg-[#525252] text-white rounded hover:bg-blue-500"
+        >
+          Просмотр
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;

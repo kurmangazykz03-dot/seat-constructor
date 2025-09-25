091e0f68b2904439f10e264f6c5e594da9b91d14
Kurman <kurmangazy.kz03@gmail.com>
2025-09-25 09:50:20 +0500

    add svg icon

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -4,7 +4,11 @@ function HomePage() {
   return (
     <div className="bg-[#FAFAFA] w-full min-h-screen flex justify-center items-center">
       <div className="bg-white w-[448px] h-[500px] rounded-[16px] drop-shadow-[0_0_15px_rgba(0,0,0,0.10)] flex items-center flex-col pt-[43px] pb-[107px] px-[48px]">
-        <div className="bg-[#E5E5E5] w-[64px] h-[64px] rounded-[16px] mb-[72px]" />
+        <div className="bg-[#E5E5E5] w-[64px] h-[64px] rounded-[16px] mb-[72px] flex justify-center items-center">
+          <svg className="w-6 h-6">
+            <use xlinkHref="#icon-home" />
+          </svg>
+        </div>
         <span className="text-[#171717] text-[24px] mb-[56px]">Конструктор схемы</span>
 
         <Link
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
+
+      symbolId: "icon-[name]",
+    }),
+  ],
+});
